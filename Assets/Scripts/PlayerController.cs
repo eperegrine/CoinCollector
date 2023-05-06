09d7d430d749590c124f81d1d033e593da4dc7b4
eperegrine <peregrineemily@gmail.com>
2023-05-06 18:30:55 +0100

    Added boost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 50f;
+    public float Speed = 500f;
+    public float BoostAmt = 5f;
+    
     private Rigidbody _rb { get; set; }
 
     private Vector2 _movement;
+    private Vector3 _movementXZ => new Vector3(_movement.x, 0, _movement.y);
     
     // Start is called before the first frame update
     void Start() => _rb = GetComponent<Rigidbody>();
 
-    private void FixedUpdate() => _rb.AddForce( new Vector3(_movement.x, 0, _movement.y) * Speed * Time.fixedDeltaTime);
+    private void FixedUpdate()
+    {
+        _rb.AddForce(_movementXZ * Speed * Time.fixedDeltaTime);
+    }
 
     public void OnMove(InputValue val) => _movement = val.Get<Vector2>();
+
+    public void OnBoost(InputValue val)
+    {
+        Debug.Log("Boost");
+        
+        _rb.AddForce(_movementXZ * BoostAmt, ForceMode.Impulse);
+    }
 }
